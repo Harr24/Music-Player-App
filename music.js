333c136f022b8c2255bd2c8560dc7ef0a3b7b9ab
Harr24 <137282048+Harr24@users.noreply.github.com>
2025-06-23 23:42:17 +0700

    Add Music Player App folder

---
+++ music.js
@@ -0,0 +1,25 @@
+let trackList = [{
+        name: "Back TO Friends",
+        path: "./music/back-to-friends.mp3",
+        img: "./images/img1.jpg",
+        singer: "Sombr",
+    },
+    {
+        name: "23",
+        path: "./music/23.mp3",
+        img: "./images/img2.jpg",
+        singer: "Diamond Eyes",
+    },
+    {
+        name: "A Thousand Years",
+        path: "./music/A-Thousand-Years.mp3",
+        img: "./images/img3.jpg",
+        singer: "Christina Perry",
+    },
+    {
+        name: "Chrystal the days",
+        path: "./music/CHRYSTAL-THE-DAYS.mp3",
+        img: "./images/img4.jpg",
+        singer: "Chrystal",
+    },
+];
